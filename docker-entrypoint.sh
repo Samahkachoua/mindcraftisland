fb8a02b19b56fc7ce3c170951040f60fb35465f0
Samahkachoua <samah.kachoua@gmail.com>
2026-07-31 21:56:50 +0300

    Add ladies program option, update registration form and update the admin panel

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan migrate --force
+php artisan migrate --force || echo "Migration failed — continuing startup anyway (this DB isn't required for the app to run)."
 
 exec "$@"
