099f3d5faf2d15452238174c1a6a84a45ed8d0e6
Samahkachoua <samah.kachoua@gmail.com>
2026-07-31 22:04:09 +0300

    Add ladies program option, update registration form and update the admin panel

--- render-build.sh
+++ render-build.sh
@@ -6,4 +6,4 @@ composer install --no-dev --optimize-autoloader
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan migrate --force || echo "Migration failed — continuing build anyway (this DB isn't required for the app to run)."
+php artisan migrate --force
